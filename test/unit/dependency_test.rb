d859fa3b6f533f229c71e8d641cc0de5c28d92f1
Jeff Koenig <jkoenig311@gmail.com>
2015-04-04 00:07:44 -0400

    add a bunch more dependency tests to check all the different ways we expect to match and not match dependencies

--- test/unit/dependency_test.rb
+++ test/unit/dependency_test.rb
@@ -5,15 +5,31 @@ class DependencyTest < UnitTest
 
     describe 'show blocks' do
 
-      it 'starting with the word dependency' do
-        dependency = "Dependency\n"+
+      match_words = %w(
+        dependent
+        dependency
+        dependencies
+        dependencys
+      )
+
+      match_words.each do |word|
+        it 'starting with the word #{word}' do
+          dependency = "#{word}\n"+
+            "This is a dependency"
+          story = stub_story_with_block dependency
+          Printer.new.get_dependency(story).must_equal dependency
+        end
+      end
+
+      it 'starting with the word dependent' do
+        dependency = "Dependent on\n"+
           "This is a dependency"
         story = stub_story_with_block dependency
         Printer.new.get_dependency(story).must_equal dependency
       end
 
-      it 'starting with the word dependent' do
-        dependency = "Dependent on\n"+
+      it 'starting with spaces then the word dependent' do
+        dependency = "  Dependent on\n"+
           "This is a dependency"
         story = stub_story_with_block dependency
         Printer.new.get_dependency(story).must_equal dependency
@@ -26,6 +42,15 @@ class DependencyTest < UnitTest
         Printer.new.get_dependency(story).must_equal dependency
       end
 
+    end
+
+    describe 'dont show blocks' do
+      it 'when dependent is not the first word' do
+        dependency = "not Dependent\n"+
+          "This is NOT a dependency"
+        story = stub_story_with_block dependency
+        Printer.new.get_dependency(story).must_equal nil
+      end
     end
 
     describe 'dependency flag' do
